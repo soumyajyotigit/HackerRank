29af216d28a2adc8e92f65febb661f9f0bb11e80
Soumyajyoti Karmakar <ksoumyajyoti@gmail.com>
2021-07-21 17:36:17 +0530

    Create ConditionalStatements.cpp

---
+++ ConditionalStatements.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string int_map[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    if (n > 9) {
+        cout << "Greater than 9" << endl;
+    }
+    else {
+        cout << int_map[n-1] << endl;
+    }
+    return 0;
+}
